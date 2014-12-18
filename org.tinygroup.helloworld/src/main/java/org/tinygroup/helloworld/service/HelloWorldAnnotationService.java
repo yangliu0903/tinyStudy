faa87afbad870a0f495b91a2fe7197f605d2fabe
aotianji1238 <aotianji1238@163.com>
2014-12-18 21:38:12 +0800

    ServiceViewMappings注解元素删除，修改helloworld服务的相关实现

--- org.tinygroup.helloworld/src/main/java/org/tinygroup/helloworld/service/HelloWorldAnnotationService.java
+++ org.tinygroup.helloworld/src/main/java/org/tinygroup/helloworld/service/HelloWorldAnnotationService.java
@@ -18,13 +18,11 @@ package org.tinygroup.helloworld.service;
 import org.tinygroup.service.annotation.ServiceComponent;
 import org.tinygroup.service.annotation.ServiceMethod;
 import org.tinygroup.service.annotation.ServiceResult;
-import org.tinygroup.service.annotation.ServiceViewMapping;
 
 @ServiceComponent()
 public class HelloWorldAnnotationService{
 	@ServiceMethod(serviceId = "sayHelloA")
 	@ServiceResult(name = "result")
-	@ServiceViewMapping("/helloworld/helloresult.page")
 	public String sayHello(String name) {
 		if (name == null) {
 			name = "world.";
