2f05347034a904c9c8a21d02ed574e6f57c35f01
aotianji1238 <aotianji1238@163.com>
2014-12-18 21:37:31 +0800

    ServiceViewMappings注解元素删除，修改四则运算服务的相关实现

--- org.tinygroup.fouroperate/src/main/java/org/tinygroup/fouroperate/service/FourOperateAnnotationService.java
+++ org.tinygroup.fouroperate/src/main/java/org/tinygroup/fouroperate/service/FourOperateAnnotationService.java
@@ -18,31 +18,26 @@ package org.tinygroup.fouroperate.service;
 import org.tinygroup.service.annotation.ServiceComponent;
 import org.tinygroup.service.annotation.ServiceMethod;
 import org.tinygroup.service.annotation.ServiceResult;
-import org.tinygroup.service.annotation.ServiceViewMapping;
 
 @ServiceComponent()
 public class FourOperateAnnotationService{
 	@ServiceMethod(serviceId = "additionWithAnno")
 	@ServiceResult(name = "result")
-	@ServiceViewMapping("/fouroperate/result.page")
 	public Double addition(Double number1,Double number2){
 		return number1+number2;
 	}
 	@ServiceMethod(serviceId = "subtractWithAnno")
 	@ServiceResult(name = "result")
-	@ServiceViewMapping("/fouroperate/result.page")
 	public Double subtraction(Double number1,Double number2){
 		return number1-number2;
 	}
 	@ServiceMethod(serviceId = "multiWithAnno")
 	@ServiceResult(name = "result")
-	@ServiceViewMapping("/fouroperate/result.page")
 	public Double multi(Double number1,Double number2){
 		return number1*number2;
 	}
 	@ServiceMethod(serviceId = "divisionWithAnno")
 	@ServiceResult(name = "result")
-	@ServiceViewMapping("/fouroperate/result.page")
 	public Double division (Double number1,Double number2){
 		return number1/number2;
 	}
