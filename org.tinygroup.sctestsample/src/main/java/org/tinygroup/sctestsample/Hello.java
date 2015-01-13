c517268bec69e524553a02acef6dcc8267e9272e
atizose <279708573@qq.com>
2015-01-13 17:33:07 +0800

    添加userhello服务

--- org.tinygroup.sctestsample/src/main/java/org/tinygroup/sctestsample/Hello.java
+++ org.tinygroup.sctestsample/src/main/java/org/tinygroup/sctestsample/Hello.java
@@ -1,8 +1,12 @@
 package org.tinygroup.sctestsample;
 
 public class Hello {
-
 	public String hello(String name) {
+
 		return "hello," + name;
+	}
+
+	public String hello(User user) {
+		return "hello," + user.getName() + ",value:" + user.getValue();
 	}
 }
