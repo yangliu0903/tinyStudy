1eb19ef214c325e0438ca8ff631fb75764dcb749
atizose <279708573@qq.com>
2015-03-19 20:18:33 +0800

    sc测试用例工程调整

--- org.tinygroup.sctestsample/src/test/java/org/tinygroup/sctestsample/test/TestServiceAsA.java
+++ org.tinygroup.sctestsample/src/test/java/org/tinygroup/sctestsample/test/TestServiceAsA.java
@@ -15,31 +15,11 @@
  */
 
 package org.tinygroup.sctestsample.test;
-import org.tinygroup.beancontainer.BeanContainerFactory;
-import org.tinygroup.cepcore.CEPCore;
-import org.tinygroup.cepcore.EventProcessor;
-import org.tinygroup.cepcorenetty.NettyCepCoreImpl;
-import org.tinygroup.cepcorenettysc.operator.ArOperator;
 import org.tinygroup.tinytestutil.AbstractTestUtil;
 public class TestServiceAsA {
 	public static void main(String[] args) {
 		AbstractTestUtil.init("applicationa.xml", true);
-//		NettyCepCoreImpl p  = BeanContainerFactory.getBeanContainer(
-//				TestServiceAsA.class.getClassLoader()).getBean(CEPCore.CEP_CORE_BEAN);
-//		p.setNodeName("asa");
-//		ArOperator aro = new ArOperator("192.168.232.54","5555","192.168.232.54","8888",10);
-//		
-//		p.setOperator(aro);
-//		p.registerEventProcessor(getEventProcessor());
-//		p.startCEPCore(p);
 	}
 	
-	public static EventProcessor getEventProcessor(){
-		EventProcessorB b = new EventProcessorB();
-		b.addServiceInfo(new ServiceB("b0"));
-		b.addServiceInfo(new ServiceB("b1"));
-		b.addServiceInfo(new ServiceB("b2"));
-		
-		return b;
-	}
+	
 }
