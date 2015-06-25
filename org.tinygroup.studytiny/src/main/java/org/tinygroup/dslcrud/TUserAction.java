e106691451a8bac4ef58bb2bb0e9a9c08e546f79
罗果 <luo_guo@icloud.com>
2015-06-25 15:40:51 +0800

    重构

--- org.tinygroup.studytiny/src/main/java/org/tinygroup/dslcrud/TUserAction.java
+++ org.tinygroup.studytiny/src/main/java/org/tinygroup/dslcrud/TUserAction.java
@@ -1,7 +1,18 @@
 /**
-* <p>Copyright: Copyright (c) 2015</p>
-* <p>Company: 恒生电子股份有限公司</p>
-*/
+ *  Copyright (c) 1997-2013, www.tinygroup.org (tinygroup@126.com).
+ *
+ *  Licensed under the GPL, Version 3.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.gnu.org/licenses/gpl.html
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.tinygroup.dslcrud;
 
 import java.util.List;
