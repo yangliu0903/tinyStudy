d49f6ef986a61939bb8c8aec532a1c5de0a93359
aotianji1238 <aotianji1238@163.com>
2015-08-14 13:53:48 +0800

    tinySample迁移webservicesample到websample目录

---
+++ websample/org.tinygroup.webservicesample/src/main/java/org/tinygroup/webservicesample/test/TimeServerPublisher.java
@@ -0,0 +1,24 @@
+/**
+ *  Copyright (c) 1997-2013, www.tinygroup.org (tinygroup@126.com).
+ *
+ *  Licensed under the GPL, Version 3.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.gnu.org/licenses/gpl.html
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.tinygroup.webservicesample.test;
+
+import javax.xml.ws.Endpoint;
+
+public class TimeServerPublisher {
+	public static void main(String[] args) {
+		Endpoint.publish("http://127.0.0.1:10100/myweb", new TimeServerImpl());
+	}
+}
