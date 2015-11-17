af63e420e74cb40038799fb0066712852a2eb396
BYSocket <qiangqiangli1993@gmail.com>
2015-11-17 14:04:49 +0800

    新增国际化示例

---
+++ template/quickstart.templatei18n/src/main/java/org/tinygroup/i18n/I18nSampleAction.java
@@ -0,0 +1,33 @@
+package org.tinygroup.i18n;
+
+
+import org.tinygroup.weblayer.WebContext;
+import org.tinygroup.weblayer.mvc.WebContextAware;
+import org.tinygroup.weblayer.mvc.annotation.Controller;
+import org.tinygroup.weblayer.mvc.annotation.RequestMapping;
+import org.tinygroup.weblayer.mvc.annotation.ResultKey;
+import org.tinygroup.weblayer.mvc.annotation.View;
+
+@Controller
+public class I18nSampleAction implements WebContextAware{
+	private WebContext webContext;
+
+	/**
+	 * 国际化访问方式：
+	 * 默认：localhost：port/pro/hello.do
+	 * 中文：localhost：port/pro/hello.do？_lang=en_US:UTF-8
+	 * 英文：localhost：port/pro/hello.do？_lang=zh_CN:UTF-8
+	 *
+	 * @return
+	 */
+	@RequestMapping(value={"/hello.do"})
+	@View(value= "/hello.page")
+	@ResultKey(value = "world")
+	public String helloI18nMethod() {
+		return "TinyTemplate I18n国际化";
+	}
+
+	public void setContext(WebContext webContext) {
+		this.webContext = webContext;
+	}
+}
